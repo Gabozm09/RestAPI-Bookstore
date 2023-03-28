d7d0817baf2fbe7509cb20dc0340dbb778da3580
Gabriel <gzava004@fiu.edu>
2023-03-28 17:06:14 -0400

    update project

--- Book-Details/src/main/java/com/api/bookdetails/repository/AuthorRepository.java
+++
@@ -1,7 +0,0 @@
-package com.api.bookdetails.repository;
-
-import com.api.bookdetails.model.Author;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface AuthorRepository extends MongoRepository<Author, String> {
-}
