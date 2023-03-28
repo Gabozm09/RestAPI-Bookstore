4c472d49ab4e20ca8e47f653b65c07bb1cd44266
Gabriel <gzava004@fiu.edu>
2023-03-28 16:48:51 -0400

    added models for author and book

---
+++ Book-Details/src/main/java/com/api/bookdetails/model/Book.java
@@ -0,0 +1,115 @@
+package com.api.bookdetails.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "books")
+public class Book {
+
+    @Id
+    private String id;
+    private String isbn;
+    private String name;
+    private String description;
+    private double price;
+    private String authorId;
+    private String genre;
+    private String publisher;
+    private int yearPublished;
+    private int copiesSold;
+
+
+    public Book(String id, String isbn, String name, String description, double price, String authorId, String genre, String publisher, int yearPublished, int copiesSold) {
+        this.id = id;
+        this.isbn = isbn;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.authorId = authorId;
+        this.genre = genre;
+        this.publisher = publisher;
+        this.yearPublished = yearPublished;
+        this.copiesSold = copiesSold;
+    }
+
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getAuthorId() {
+        return authorId;
+    }
+
+    public void setAuthorId(String authorId) {
+        this.authorId = authorId;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+
+    public int getYearPublished() {
+        return yearPublished;
+    }
+
+    public void setYearPublished(int yearPublished) {
+        this.yearPublished = yearPublished;
+    }
+
+    public int getCopiesSold() {
+        return copiesSold;
+    }
+
+    public void setCopiesSold(int copiesSold) {
+        this.copiesSold = copiesSold;
+    }
+}
