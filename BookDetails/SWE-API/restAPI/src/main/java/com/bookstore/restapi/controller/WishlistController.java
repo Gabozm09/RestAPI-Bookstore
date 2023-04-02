51ea93ab42541a8ef7dcb35f2b2773da4f6058a3
BuildTools <unconfigured@null.spigotmc.org>
2023-04-02 15:14:08 -0400

    Adding Wishlist to models

---
+++ BookDetails/SWE-API/restAPI/src/main/java/com/bookstore/restapi/controller/WishlistController.java
@@ -0,0 +1,10 @@
+package com.bookstore.restapi.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+public class WishlistController {
+
+}
