ca2f1d502640a8d8e0f6cfa58798d2bd1f826914
Gabriel <gzava004@fiu.edu>
2023-04-02 20:24:55 -0400

    creating Book Browsing and Sorting

---
+++ BookBrowsingAndSorting/BookSearch-Sort/src/main/java/com/api/booksearchsort/BookSearchSortApplication.java
@@ -0,0 +1,13 @@
+package com.api.booksearchsort;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookSearchSortApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookSearchSortApplication.class, args);
+    }
+
+}
