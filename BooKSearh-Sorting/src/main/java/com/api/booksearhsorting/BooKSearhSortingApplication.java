3eadad6c26eb58613af4860bce1b33108775ef0c
Gabriel <gzava004@fiu.edu>
2023-04-03 00:02:22 -0400

    created controller, services, repository and model of book search-sorting

---
+++ BooKSearh-Sorting/src/main/java/com/api/booksearhsorting/BooKSearhSortingApplication.java
@@ -0,0 +1,13 @@
+package com.api.booksearhsorting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooKSearhSortingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BooKSearhSortingApplication.class, args);
+    }
+
+}
---
+++ BooKSearh-Sorting/src/test/java/com/api/booksearhsorting/BooKSearhSortingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.booksearhsorting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooKSearhSortingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
