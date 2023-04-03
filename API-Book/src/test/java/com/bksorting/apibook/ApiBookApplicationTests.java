1fdc3b281bcc644c699d5a3b36ede4c76872612b
Gabriel <gzava004@fiu.edu>
2023-04-02 20:17:03 -0400

    creating Book Browsing and Sorting

---
+++ API-Book/src/test/java/com/bksorting/apibook/ApiBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bksorting.apibook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiBookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
