4f53c718cfa01a6fe9264219ef62c6e9400868f8
jonzappola <55258519+jonzappola@users.noreply.github.com>
2023-04-15 16:05:37 -0400

    Update Book.java

--- wishlist/src/main/java/com/api/wishlist/model/Book.java
+++ wishlist/src/main/java/com/api/wishlist/model/Book.java
@@ -8,28 +8,12 @@ public class Book {
 
     @Id
     private String id;
-    private String isbn;
     private String name;
-    private String description;
-    private double price;
-    private String authorId;
-    private String genre;
-    private String publisher;
-    private int yearPublished;
-    private int copiesSold;
 
 
-    public Book(String id, String isbn, String name, String description, double price, String authorId, String genre, String publisher, int yearPublished, int copiesSold) {
+    public Book(String id, String name) {
         this.id = id;
-        this.isbn = isbn;
         this.name = name;
-        this.description = description;
-        this.price = price;
-        this.authorId = authorId;
-        this.genre = genre;
-        this.publisher = publisher;
-        this.yearPublished = yearPublished;
-        this.copiesSold = copiesSold;
     }
 
 
@@ -41,14 +25,6 @@ public class Book {
         this.id = id;
     }
 
-    public String getIsbn() {
-        return isbn;
-    }
-
-    public void setIsbn(String isbn) {
-        this.isbn = isbn;
-    }
-
     public String getName() {
         return name;
     }
@@ -56,60 +32,5 @@ public class Book {
     public void setName(String name) {
         this.name = name;
     }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public double getPrice() {
-        return price;
-    }
-
-    public void setPrice(double price) {
-        this.price = price;
-    }
-
-    public String getAuthorId() {
-        return authorId;
-    }
-
-    public void setAuthorId(String authorId) {
-        this.authorId = authorId;
-    }
-
-    public String getGenre() {
-        return genre;
-    }
-
-    public void setGenre(String genre) {
-        this.genre = genre;
-    }
-
-    public String getPublisher() {
-        return publisher;
-    }
-
-    public void setPublisher(String publisher) {
-        this.publisher = publisher;
-    }
-
-    public int getYearPublished() {
-        return yearPublished;
-    }
-
-    public void setYearPublished(int yearPublished) {
-        this.yearPublished = yearPublished;
-    }
-
-    public int getCopiesSold() {
-        return copiesSold;
-    }
-
-    public void setCopiesSold(int copiesSold) {
-        this.copiesSold = copiesSold;
-    }
 }
+
