ff8ffca103cf679cb6efd0b40b26d76b42e2619b
jonzappola <55258519+jonzappola@users.noreply.github.com>
2023-04-15 16:07:05 -0400

    Update Book.java

--- wishlist/src/main/java/com/api/wishlist/model/Book.java
+++ wishlist/src/main/java/com/api/wishlist/model/Book.java
@@ -9,7 +9,7 @@ public class Book {
     @Id
     private String id;
     private String name;
-
+    private String title; 
 
     public Book(String id, String name) {
         this.id = id;
@@ -23,6 +23,13 @@ public class Book {
 
     public void setId(String id) {
         this.id = id;
+    }
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
     }
 
     public String getName() {
